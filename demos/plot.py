d009bd3e74c05c4b0ef1d0e613de9dfa34954a87
Sebastian Hirnschall <sebastian@hirnschall.net>
2023-12-07 15:53:36 +0100

    simulating rc network

--- demos/plot.py
+++ demos/plot.py
@@ -6,8 +6,8 @@ import numpy as np
 # read the file test_ode.txt
 data = np.loadtxt('test_ode_ie.txt')
 # plot the data
-plt.plot(data[:,0],data[:,1],label='y(t)')
-plt.plot(data[:,0],data[:,2],label='y\'(t)')
+plt.plot(data[:,0],data[:,1],label='U_0(t)')
+plt.plot(data[:,0],data[:,2],label='U_C(t)')
 plt.xlabel('t')
 plt.ylabel('y(t), y\'(t)')
 plt.legend()
--- demos/test_ode.cc
+++ demos/test_ode.cc
@@ -24,8 +24,9 @@ class MassSpring : public NonlinearFunction
 };
 
 class RCNetwork : public NonlinearFunction {
+  public:
     double R = 1; // Ohm
-    double C = 1e-3; // Farad
+    double C = 0.01; // Farad
 
     size_t DimX() const override {
         return 2;
@@ -40,11 +41,11 @@ class RCNetwork : public NonlinearFunction {
         f(1) = (std::cos(100 * M_PI * x(0)) - x(1)) / (R * C);
     }
     
-    void EvaluateDeriv (VectorView<double> x, MatrixView<double, ColMajor> df) const override {
+    void EvaluateDeriv (VectorView<double> x, MatrixView<double> df) const override {
         df = 0.0;
         df(0, 0) = 0;
         df(1, 1) = -1/(R*C);
-        df(1, 0) = -1/(R*C) * 10 * M_PI * sin(10*M_PI*x(0));
+        df(1, 0) = -1/(R*C) * 100 * M_PI * sin(100*M_PI*x(0));
     }
 };
 
@@ -52,10 +53,10 @@ class RCNetwork : public NonlinearFunction {
 
 int main()
 {
-  double tend = 4*M_PI;
+  double tend = 0.3;
   int steps = 1000;
   Vector<> y(2); 
-  y(0) = 1;
+  y(0) = 0;
   y(1) = 0;
   auto rhs = make_shared<RCNetwork>();
 
@@ -65,7 +66,7 @@ int main()
   //write steps from SolveODE_EE to file
   ofstream out("test_ode_ie.txt");
   SolveODE_IE(tend, steps, y, rhs,
-              [&out](double t, VectorView<double> y) { out << t << "  " << y(0) << " " << y(1) << endl; }); 
+              [&out](double t, VectorView<double> y) { out << t << "  " << std::cos(100 * M_PI * t) << " " << y(1) << endl; }); 
   out.close();
 
 }
